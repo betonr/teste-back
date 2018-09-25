2f3339cd20d6a0f45ad9f5820bcd30163eca1ca2
Beto Noronha <carlos.noronha@fatec.sp.gov.br>
2018-09-25 15:51:59 -0300

    finished docs

--- dockers/setupDatabase.sh
+++ dockers/setupDatabase.sh
@@ -5,6 +5,6 @@ fi
 docker exec apiauth python manage.py db migrate
 docker exec apiauth python manage.py db upgrade
 
-docker exec apiauth apidoc -i /routes/doc/config.yml -o public/docs/index.html
+docker exec apiauth apidoc -i routes/doc/config.yml -o public/docs/index.html
 
 docker exec mysql bash -c "mysql -u root -proot auth < ./users.sql"
